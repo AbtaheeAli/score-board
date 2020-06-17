d8ab4a57a1cfe176c3785cfe21c32f6ca9a86296
Abtahee Ali <abtaheeali@gmail.com>
2020-06-17 15:29:18 -0400

    Teams are able to add and subtract points

--- public/main.js
+++ public/main.js
@@ -1,7 +1,36 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let teamOne = 0
+let teamTwo = 0
+function handleAddingPointsTeamOne() {
+  const valueCount = document.querySelector('.teamOneScore')
+  teamOne++
+  valueCount.textContent = `${teamOne}`
 }
 
+function handleSubPointsTeamOne() {
+  const valueCount = document.querySelector('.teamOneScore')
+  teamOne--
+  valueCount.textContent = `${teamTwo}`
+}
+
+function handleAddingPointsTeamTwo() {
+  const valueCount = document.querySelector('.teamTwoScore')
+  teamTwo++
+  valueCount.textContent = `${teamTwo}`
+}
+
+function handleSubPointsTeamTwo() {
+  const valueCount = document.querySelector('.teamTwoScore')
+  teamTwo--
+  valueCount.textContent = `${teamTwo}`
+}
+const main = () => {
+  const scoreAddTeamOne = document.querySelector('.team1 .add')
+  const scoreAddTeamTwo = document.querySelector(' .team2 .add')
+  scoreAddTeamOne.addEventListener('click', handleAddingPointsTeamOne)
+  scoreAddTeamTwo.addEventListener('click', handleAddingPointsTeamTwo)
+  const scoreSubTeamOne = document.querySelector(' .team1 .subtract')
+  const scoreSubTeamTwo = document.querySelector('.team2 .subtract')
+  scoreSubTeamOne.addEventListener('click', handleSubPointsTeamOne)
+  scoreSubTeamTwo.addEventListener('click', handleSubPointsTeamTwo)
+}
 document.addEventListener('DOMContentLoaded', main)
