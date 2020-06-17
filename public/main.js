c1571bc3459b09e2ca91db5aee62a9c8041cee74
Abtahee Ali <abtaheeali@gmail.com>
2020-06-17 18:02:16 -0400

    Team names can be updated

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 let teamOne = 0
 let teamTwo = 0
+
 function handleAddingPointsTeamOne() {
   const valueCount = document.querySelector('.teamOneScore')
   teamOne++
@@ -23,14 +24,34 @@ function handleSubPointsTeamTwo() {
   teamTwo--
   valueCount.textContent = `${teamTwo}`
 }
+
+function updateTeamOneName(event) {
+  console.log('tacotuse')
+  const teamOneLog = document.querySelector('.team1 h2')
+  teamOneLog.textContent = event.target.value
+}
+
+function updateTeamTwoName(event) {
+  const teamTwoLog = document.querySelector('.team2 h2')
+  teamTwoLog.textContent = event.target.value
+}
 const main = () => {
   const scoreAddTeamOne = document.querySelector('.team1 .add')
   const scoreAddTeamTwo = document.querySelector(' .team2 .add')
+
   scoreAddTeamOne.addEventListener('click', handleAddingPointsTeamOne)
   scoreAddTeamTwo.addEventListener('click', handleAddingPointsTeamTwo)
+
   const scoreSubTeamOne = document.querySelector(' .team1 .subtract')
   const scoreSubTeamTwo = document.querySelector('.team2 .subtract')
+
   scoreSubTeamOne.addEventListener('click', handleSubPointsTeamOne)
   scoreSubTeamTwo.addEventListener('click', handleSubPointsTeamTwo)
+
+  const teamOneinput = document.querySelector('.team1 input')
+  teamOneinput.addEventListener('input', updateTeamOneName)
+
+  const teamTwoinput = document.querySelector('.team2 input')
+  teamTwoinput.addEventListener('input', updateTeamTwoName)
 }
 document.addEventListener('DOMContentLoaded', main)
